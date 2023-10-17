19f053b89f5810a1a007f4288fff81f12c5c77c7
Charles Goddard <chargoddard@gmail.com>
2023-10-16 22:09:27 -0700

    Add new reward model post

--- code/train-reward-model.py
+++ code/train-reward-model.py
@@ -10,7 +10,6 @@ def tokenize_fun(tokenizer: transformers.PreTrainedTokenizerBase, max_length: in
     def do_tok(name: str, row: Dict, suffix: str):
         tokenized = tokenizer(
             row[name],
-            padding=True,
             max_length=max_length,
             truncation=True,
         )
@@ -20,6 +19,11 @@ def tokenize_fun(tokenizer: transformers.PreTrainedTokenizerBase, max_length: in
         }
 
     def f(row):
+        # set up to handle three dataset formats:
+        # 1. {"input_text": str, "labels": int}
+        # 2. {"chosen": str, "rejected": str}
+        # 3. {"prompt": str, "chosen": str, "rejected": str}
+
         if "prompt" in row:
             row["chosen"] = row["prompt"] + row["chosen"]
             row["rejected"] = row["prompt"] + row["rejected"]
@@ -48,12 +52,18 @@ def main(
     sequence_len: int = 1024,
     lr: float = 0.0001,
     epochs: int = 1,
+    regression: bool = False,
 ):
-    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path)
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(
+        model_path, num_labels=1 if regression else 2
+    )
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
-    tokenizer.truncation_side = "right"
+    tokenizer.truncation_side = "left"  # we care about the most recent tokens
 
     if isinstance(model, transformers.GPT2ForSequenceClassification):
+        tokenizer.pad_token = tokenizer.eos_token
+        model.config.pad_token_id = tokenizer.pad_token_id
+    elif isinstance(model, transformers.LlamaForSequenceClassification):
         tokenizer.pad_token = tokenizer.eos_token
         model.config.pad_token_id = tokenizer.pad_token_id
     print(tokenizer.special_tokens_map)
@@ -62,7 +72,11 @@ def main(
     if "train" in ds:
         ds = ds["train"]
 
-    ds = ds.map(tokenize_fun(tokenizer, sequence_len))
+    ds = ds.map(
+        tokenize_fun(tokenizer, sequence_len),
+        num_proc=16,
+        remove_columns=ds.column_names,
+    )
 
     if eval_dataset:
         ds_e = datasets.load_dataset(eval_dataset)
@@ -73,25 +87,32 @@ def main(
     else:
         ds_e = None
 
-    arg_type = trl.RewardConfig if use_trl else transformers.TrainingArguments
-    train_args = arg_type(
-        output_dir=out_path,
-        per_device_train_batch_size=micro_batch_size,
-        per_device_eval_batch_size=micro_batch_size,
-        bf16=True,
-        lr_scheduler_type="cosine",
-        optim="adamw_torch_fused",
-        warmup_steps=10,
-        learning_rate=lr,
-        seed=seed,
-        evaluation_strategy="steps" if ds_e is not None else "no",
-        eval_steps=100,
-        save_steps=1000,
-        report_to="wandb",
-        logging_steps=1,
-        num_train_epochs=epochs,
-        max_length=sequence_len,
-    )
+    train_kwargs = {
+        "output_dir": out_path,
+        "per_device_train_batch_size": micro_batch_size,
+        "per_device_eval_batch_size": micro_batch_size,
+        "bf16": True,
+        "lr_scheduler_type": "cosine",
+        "optim": "adamw_torch_fused",
+        "warmup_steps": 200,
+        "learning_rate": lr,
+        "seed": seed,
+        "evaluation_strategy": "steps" if ds_e is not None else "no",
+        "eval_steps": 100,
+        "save_steps": 1000,
+        "report_to": "wandb",
+        "logging_steps": 1,
+        "num_train_epochs": epochs,
+        "save_total_limit": 5,
+        "load_best_model_at_end": True,
+        "remove_unused_columns": False,
+    }
+    arg_type = transformers.TrainingArguments
+    if use_trl:
+        arg_type = trl.RewardConfig
+        train_kwargs["max_length"] = sequence_len
+        train_kwargs["gradient_checkpointing"] = False
+    train_args = arg_type(**train_kwargs)
 
     trainer_type = trl.RewardTrainer if use_trl else transformers.Trainer
     trainer = trainer_type(
@@ -101,6 +122,7 @@ def main(
         train_dataset=ds,
         eval_dataset=ds_e,
     )
+
     trainer.train()
     model.save_pretrained(out_path, safe_serialization=True)
 
